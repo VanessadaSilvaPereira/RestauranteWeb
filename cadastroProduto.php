fd42cfa4304d2a7fb7231c26e6a11bf276cb16dc
Vanessa Da Silva Pereira <181710106@senacinformatica.br>
2019-05-14 11:55:55 -0300

    alteração na página de configurações

--- cadastroProduto.php
+++ cadastroProduto.php
@@ -8,8 +8,10 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Cadastro</title>
+         <link href="CSS/Estilos.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     </head>
-    <body>
+    <body class="bodyFundoMadeira">
         <?php
         // put your code here
         ?>
--- config.php
+++ config.php
@@ -8,13 +8,31 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Configurações</title>
-          <link href="CSS/Estilos.css" rel="stylesheet" type="text/css">
-         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link href="CSS/Estilos.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <h1  class="titulo">Configurações</h1>
-    </head>
-    <body class="bodyFundoMadeira">
-        
-        <h1>Mesa</h1>
-        <button id="botaoCadastro" onclick="window.location= 'cadastroProduto.php'" >Cadastrar Produto</button>
-    </body>
+</head>
+<body class="bodyFundoMadeira">
+
+    <div><label>Selecionar Mesa</label></div>
+    <div><select></select></div>
+    
+    
+    <div style="margin-top:5%; margin-bottom: 5%">
+    <div><label>Trocar Senha</label></div>
+    <div><label>Senha atual:</label>
+     <input type="text"></div>
+    <label>Nova Senha:</label>
+     <input type="text">
+    </div>
+     
+
+     <div><label>Cadastrar Categoria:</label></div>
+     <input type="text" style="margin-left: 25%;width: 15%;">
+     
+     
+
+
+    <button id="botaoCadastro" onclick="window.location = 'cadastroProduto.php'" >Cadastrar Produto</button>
+</body>
 </html>
