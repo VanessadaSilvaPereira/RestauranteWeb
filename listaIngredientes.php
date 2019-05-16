c48b4691893fb006eb511aa6f2bfc86ce7f37aa4
Vanessa da Silva Pereira <vanessa.pereira787@gmail.com>
2019-05-16 12:09:56 -0300

    listando ingredientes

--- listaIngredientes.php
+++ listaIngredientes.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
-
+<?php
+require './dao/clsIngredienteDAO.php';
+require './dao/clsConexao.php';
+require './model/clsIngrediente.php';
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -7,10 +11,29 @@
         <title></title>
          <link href="CSS/Estilos.css" rel="stylesheet" type="text/css">
          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+         
     </head>
     <body>
-        <?php
-        
+        <h1 class="titulo">Ingredientes</h1>
+         <?php
+            $lista = IngredienteDAO::getIngredientes();
+            
+            if ( $lista->count()==0){
+                echo '<h2><b>Nenhum Ingrediente cadastrado</b></h2>';
+            }else {
         ?>
+        
+        <div id="divListaIngrediente">
+            <?php 
+                foreach ($lista as $ingrediente) {
+                    echo '<div id="ingrediente"><input type="checkbox" value="" />'.$ingrediente->getNome().'</div>';            
+                }
+            ?>
+        </div>
+      
+        <?php
+          }
+        ?>
+        
     </body>
 </html>
